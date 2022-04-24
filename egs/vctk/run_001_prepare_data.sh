b3431d8d8694996339b3acae983bf3a2d6d0067f
Dami Kazeem <dkazeem@c01.clsp.jhu.edu>
2022-04-24 10:47:17 -0400

    updates for vctk to run_001_prepare_data.sh

--- egs/vctk/run_001_prepare_data.sh
+++ egs/vctk/run_001_prepare_data.sh
@@ -10,20 +10,15 @@ set -e
 stage=1
 config_file=default_config.sh
 
-# DAMI - update parse_options.sh in utils/? No. 
 . parse_options.sh || exit 1;
-# DAMI - update datapath.sh in root of folder.
 . datapath.sh 
 
 
 if [ $stage -le 1 ];then
   # Prepare the VoxCeleb2 dataset for training.
-  # local/make_voxceleb2cat.pl $voxceleb2_root dev 16 data/voxceleb2cat_train
-
-  # Prepare the VCTK dataset for training.
-  # DAMI - TODO: need to update this to data/#/train with a loop 
-  # to go through #'s from 0 to n-1 kfolds. first, this test run.
-  local/make_vctk.py $vctk_root dev 16 data/0/train
+  local/make_voxceleb2cat.pl $voxceleb2_root dev 16 data/voxceleb2cat_train
+  # DAMI - TODO: update here
+  # local/make_vctk.py $vctk_root dev 16 data/0/train
 fi
 
 if [ $stage -le 2 ];then
@@ -32,4 +27,7 @@ if [ $stage -le 2 ];then
   # local/make_voxceleb1_oeh.pl $voxceleb1_root data
   # Use this for the newer version of voxceleb1:
   local/make_voxceleb1_v2_oeh.pl $voxceleb1_root data
+
+  # DAMI - TODO: update here
+  # local/make_vctk.py $vctk_root dev 16 data/0/train
 fi
