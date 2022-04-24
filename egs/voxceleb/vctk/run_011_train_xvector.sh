c36fd74c96e80af07ea03c5b2411475b02b5324c
Dami Kazeem <dkazeem@c01.clsp.jhu.edu>
2022-04-24 19:59:57 -0400

    reset gpu from 4 to 1. rerun.

--- egs/voxceleb/vctk/run_011_train_xvector.sh
+++ egs/voxceleb/vctk/run_011_train_xvector.sh
@@ -8,7 +8,9 @@
 set -e
 
 stage=1
-ngpu=4
+# DAMI - TODO: set gpu usage accordingly - default was 4, recommended is 1 for this task
+# ngpu=4
+ngpu=1
 config_file=default_config.sh
 resume=false
 interactive=false
